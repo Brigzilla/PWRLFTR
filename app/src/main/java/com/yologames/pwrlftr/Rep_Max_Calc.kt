fb39f14167b0d668d48ff8705a3a6744d6f14ee7
Brigzilla <matbrignall@hotmail.co.uk>
2023-05-11 17:46:07 +0100

    Test commit. Connected buttons in Rep_Max_Calc.kt to their view

--- app/src/main/java/com/yologames/pwrlftr/Rep_Max_Calc.kt
+++ app/src/main/java/com/yologames/pwrlftr/Rep_Max_Calc.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 class Rep_Max_Calc : Fragment() {
+
+    lateinit var input_reps_box : TextView
+    lateinit var input_weight_box: TextView
+    lateinit var calculate_button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,6 +25,22 @@ class Rep_Max_Calc : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rep__max__calc, container, false)
+
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        calculate_button  = view.findViewById(R.id.calculate_1rm_button)
+        input_reps_box = view.findViewById(R.id.enter_reps_textbox)
+        input_weight_box = view.findViewById(R.id.enter_weight_textbox)
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+    fun calculateOneRepMax(weight: Int, reps: Int): Int{
+        var one_rm: Int = 0
+
+       return one_rm
+    }
 }
