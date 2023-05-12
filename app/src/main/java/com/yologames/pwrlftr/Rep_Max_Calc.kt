c1b081b2cea059b3e647ecb2c00ba68d7e7121d4
Brigzilla <matbrignall@hotmail.co.uk>
2023-05-12 09:18:23 +0100

    Most core work for the 1RM Calc is complete

--- app/src/main/java/com/yologames/pwrlftr/Rep_Max_Calc.kt
+++ app/src/main/java/com/yologames/pwrlftr/Rep_Max_Calc.kt
@@ -1,6 +1,7 @@
 package com.yologames.pwrlftr
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,8 @@ class Rep_Max_Calc : Fragment() {
 
     lateinit var input_reps_box : TextView
     lateinit var input_weight_box: TextView
+
+    lateinit var output_text_view: TextView
     lateinit var calculate_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,17 +33,76 @@ class Rep_Max_Calc : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        calculate_button  = view.findViewById(R.id.calculate_1rm_button)
+        calculate_button  = view.findViewById(R.id.calculate_button)
         input_reps_box = view.findViewById(R.id.enter_reps_textbox)
         input_weight_box = view.findViewById(R.id.enter_weight_textbox)
+        output_text_view = view.findViewById(R.id.output_text_view_1)
+
+        output_text_view.visibility = View.INVISIBLE
+
+        calculate_button.setOnClickListener{
+           output_text_view.text = calculateOneRepMax(input_weight_box.text.toString().toFloat(), input_reps_box.text.toString().toFloat()).toString()
+
+        }
+
+        input_weight_box.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
+                if (input_weight_box.text.isNotEmpty() || input_reps_box.text.isNotEmpty()) {
+                    output_text_view.text = calculateOneRepMax(
+                        input_weight_box.text.toString().toFloat(),
+                        input_reps_box.text.toString().toFloat()
+                    ).toString()
+                }
+
+                return@OnKeyListener true
+            }
+            false
+        })
+
+        input_reps_box.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
+                if (input_weight_box.text.isNotEmpty() || input_reps_box.text.isNotEmpty()) {
+                    output_text_view.text = calculateOneRepMax(
+                        input_weight_box.text.toString().toFloat(),
+                        input_reps_box.text.toString().toFloat()
+                    ).toString()
+                }
+
+                return@OnKeyListener true
+            }
+            false
+        })
         super.onViewCreated(view, savedInstanceState)
 
     }
 
 
-    fun calculateOneRepMax(weight: Int, reps: Int): Int{
-        var one_rm: Int = 0
 
+    fun calculateOneRepMax(weight: Float, reps: Float): Float{
+        var one_rm: Float = weight
+        var t_reps = reps
+        var offset : Float = 0.0f
+        var multiplier: Float = 0.97f
+        while (t_reps > 1)
+        {
+            multiplier -= offset
+            one_rm /= multiplier
+            offset -= 0.00001f
+            t_reps--
+        }
+        output_text_view.visibility = View.VISIBLE
        return one_rm
     }
 }
+// Prev *.97
+//outputBox_01.text = " 1 Rep Max - "+(string_02).toString()
+//outputBox_02.text = " 2 Rep Max - "+(BigDecimal(string_02.toDouble()*0.95f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
+//outputBox_03.text = " 3 Rep Max - "+(BigDecimal(string_02.toDouble()*0.92f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
+//outputBox_04.text = " 4 Rep Max - "+(BigDecimal(string_02.toDouble()*0.90f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
+//outputBox_05.text = " 5 Rep Max - "+(BigDecimal(string_02.toDouble()*0.86f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
+//outputBox_06.text = " 6 Rep Max - "+(BigDecimal(string_02.toDouble()*0.84f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
+//outputBox_07.text = " 7 Rep Max - "+(BigDecimal(string_02.toDouble()*0.82f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
+//outputBox_08.text = " 8 Rep Max - "+(BigDecimal(string_02.toDouble()*0.80f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
+//outputBox_09.text = " 9 Rep Max - "+(BigDecimal(string_02.toDouble()*0.775f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
+//outputBox_10.text = "10 Rep Max - "+(BigDecimal(string_02.toDouble()*0.75f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
+//outputBox_12.text = "12 Rep Max - "+(BigDecimal(string_02.toDouble()*0.69f*(1+offsetValue)).setScale(2, RoundingMode.HALF_EVEN)).toString()
