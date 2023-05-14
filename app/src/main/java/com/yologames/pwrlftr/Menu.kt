da128c681b100a8d46a184b63d071097b8c7f918
Brigzilla <matbrignall@hotmail.co.uk>
2023-05-14 14:27:34 +0100

    Viewpager2 is functional and works very well.
    Tutorial from:
    https://www.youtube.com/watch?v=WbpKInkd0YQ

--- app/src/main/java/com/yologames/pwrlftr/Menu.kt
+++ app/src/main/java/com/yologames/pwrlftr/Menu.kt
@@ -1,17 +1,29 @@
 package com.yologames.pwrlftr
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.findFragment
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 
 
 class Menu : Fragment() {
     lateinit var b_onerm: Button
     lateinit var r_repmx: Button
+
+    val NUM_PAGES : Int = 2
+    lateinit var viewpager : ViewPager2
+    lateinit var tabLayout : TabLayout
+    lateinit var adapter : FragmentPageAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,20 +35,42 @@ class Menu : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        b_onerm = view.findViewById(R.id.one_rm_calc)
-        r_repmx = view.findViewById(R.id.rep_max_calc)
+
+        tabLayout = view.findViewById(R.id.tab_layout)
+        viewpager = view.findViewById(R.id.pager)
+        adapter = FragmentPageAdapter(requireActivity().supportFragmentManager, lifecycle)
+        tabLayout.addTab(tabLayout.newTab().setText("One Rep Max"))
+        tabLayout.addTab(tabLayout.newTab().setText("Rep Maxes"))
+        viewpager.adapter = adapter
+
+        tabLayout.addOnTabSelectedListener(object  : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewpager.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        }
+        )
+        viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+
 
         setOnClickListeners()
         super.onViewCreated(view, savedInstanceState)
     }
 
+
     fun setOnClickListeners(){
-        b_onerm.setOnClickListener{
-            findNavController().navigate(R.id.action_menu_to_one_rm_calc)
-        }
-        r_repmx.setOnClickListener{
-            findNavController().navigate(R.id.action_menu_to_rep_Max_Calc)
-        }
+
     }
 
 
