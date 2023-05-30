4e7d0554c0df26fc6a00fc934dcffd44f97e982f
Brigzilla <matbrignall@hotmail.co.uk>
2023-05-30 16:58:49 +0100

    Revert "Program Generator fragment initial commit"
    This reverts commit bf6b49beff621325899b2176af351e16465dfa0a.

--- app/src/main/java/com/yologames/pwrlftr/FragmentPageAdapter.kt
+++ app/src/main/java/com/yologames/pwrlftr/FragmentPageAdapter.kt
@@ -4,7 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.adapter.FragmentViewHolder
 
 class FragmentPageAdapter (
     fragmentManager: FragmentManager,
@@ -28,6 +27,4 @@ class FragmentPageAdapter (
             return Rep_Max_Calc()
         }
     }
-
-
 }
--- app/src/main/java/com/yologames/pwrlftr/ProgramGeneratorViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.yologames.pwrlftr
-
-import androidx.lifecycle.ViewModel
-
-class ProgramGeneratorViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
