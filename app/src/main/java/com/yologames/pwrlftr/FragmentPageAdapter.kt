bf6b49beff621325899b2176af351e16465dfa0a
Brigzilla <matbrignall@hotmail.co.uk>
2023-05-30 16:37:38 +0100

    Program Generator fragment initial commit

--- app/src/main/java/com/yologames/pwrlftr/FragmentPageAdapter.kt
+++ app/src/main/java/com/yologames/pwrlftr/FragmentPageAdapter.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.adapter.FragmentViewHolder
 
 class FragmentPageAdapter (
     fragmentManager: FragmentManager,
@@ -22,4 +23,6 @@ class FragmentPageAdapter (
             Rep_Max_Calc()
         }
     }
+
+
 }
---
+++ app/src/main/java/com/yologames/pwrlftr/ProgramGeneratorViewModel.kt
@@ -0,0 +1,7 @@
+package com.yologames.pwrlftr
+
+import androidx.lifecycle.ViewModel
+
+class ProgramGeneratorViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
