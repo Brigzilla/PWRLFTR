66f1ac4aac4c486565d9e023007b34a58fb35181
Brigzilla <matbrignall@hotmail.co.uk>
2023-06-17 22:27:39 +0100

    I've fixed the database rollback issue...

--- app/src/main/java/com/yologames/pwrlftr/ProgramGenerator.kt
+++ app/src/main/java/com/yologames/pwrlftr/ProgramGenerator.kt
@@ -53,7 +53,7 @@ class ProgramGenerator : Fragment() {
         val database = Room.databaseBuilder(
             requireContext(),
             SessionDatabase::class.java, "session_database"
-        ).allowMainThreadQueries().build()
+        ).fallbackToDestructiveMigration().allowMainThreadQueries().build()
         sessionDao = database.sessionDao()
         queryDatabaseSize()
         can_init = true
